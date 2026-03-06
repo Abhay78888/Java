da9334d4b349ecdcc487bb36127fe921b7d2d7e4
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-03-06 23:33:02 +0530

    Time: 4 ms (99.78%) | Memory: 46 MB (52.29%) - LeetSync

---
+++ Leetcode/9-palindrome-number/palindrome-number.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int temp=x;
+        
+        int rev=0;
+        if(x<0){
+            return false;
+        }
+        while(x>0){
+            int rem = x%10;
+            rev = rev*10 + rem;
+            x=x/10;
+        }
+
+      
+        return temp == rev;
+
+}
+}
+        
+    
