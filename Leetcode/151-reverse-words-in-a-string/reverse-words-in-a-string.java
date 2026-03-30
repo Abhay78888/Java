dfaf063d7ae1ecc21a8f247e69741327f381121d
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-03-30 23:54:38 +0530

    Time: 7 ms (65.00%) | Memory: 45.6 MB (21.91%) - LeetSync

---
+++ Leetcode/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+
+        s = new StringBuilder(s).reverse().toString();
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder word = new StringBuilder();
+
+            while (i < n && s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
+                i++;
+            }
+
+
+            word.reverse();
+
+            if (word.length() > 0) {
+                ans.append(" ").append(word);
+            }
+        }
+
+
+        return ans.substring(1);
+        
+    }
+}
