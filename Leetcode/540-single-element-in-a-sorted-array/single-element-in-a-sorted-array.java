65fee0cd501710f1a3d73f0ce747d55693c42de1
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-04-18 21:58:07 +0530

    Time: 1 ms (25.63%) | Memory: 53.2 MB (10.94%) - LeetSync

---
+++ Leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int ans =0;
+        for(int i =0;i<arr.length;i++){
+            ans = ans^arr[i];
+        }
+        return ans;
+        
+
+    
+    }
+}
