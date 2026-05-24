7e0bc24a72cd8c2c5fc71be9e5a7a025a050228c
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-05-25 00:40:37 +0530

    Time: 94 ms (81.49%) | Memory: 51.1 MB (51.91%) - LeetSync

---
+++ Leetcode/204-count-primes/count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n==0 || n==1){
+            return 0;
+        }
+        boolean [] prime = new boolean[n];
+        Arrays.fill(prime,true);
+        prime[0]=false;
+        prime[1]=false;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j =i*i;j<n;j=j+i){
+                    prime[j]=false;
+                }
+            }
+        }
+        int count =0;
+        for(int i =0;i<n;i++){
+            if(prime[i]==true){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
