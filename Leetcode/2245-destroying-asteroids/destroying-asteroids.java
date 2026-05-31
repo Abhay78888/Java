f4c7812bdf61243bb9b85155acb52c0bcc4bca24
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-05-31 22:45:39 +0530

    Time: 26 ms (81.52%) | Memory: 107.6 MB (16.40%) - LeetSync

---
+++ Leetcode/2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] arr) {
+        Arrays.sort(arr);
+        long ans = mass;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>ans){
+                return false;
+            }
+            ans=ans+arr[i];
+        }
+        return true;
+    }
+}
