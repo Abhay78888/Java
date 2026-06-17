63ee01156cd28b2160db070601c473180ee7936b
Abhay_0020 <abhaybhadauria8999@gmail.com>
2026-06-17 19:38:53 +0530

    Time: 3 ms (96.57%) | Memory: 42.3 MB (29.55%) - LeetSync

--- Leetcode/633-sum-of-square-numbers/sum-of-square-numbers.java
+++ Leetcode/633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for(long i =0;i*i<=c;i++){
-            double b=Math.sqrt(c-i*i);
-            if(b==(int)b){
+        long left =0;
+        long right=(long)Math.sqrt(c);
+        while(left<=right){
+            long sum = left*left+right*right;
+            if(sum==c){
                 return true;
             }
-            
+            else if(sum>c){
+                right--;
+            }
+            left++;
         }
         return false;
         
